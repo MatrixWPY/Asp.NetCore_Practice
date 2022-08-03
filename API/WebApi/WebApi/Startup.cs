c7b9102cc1416499d85e3360cb792244abb17a96
MatrixWPY <matrix.wpy@gmail.com>
2022-08-03 20:56:06 +0800

    [Add]WebAPI 1.導入Swagger套件設定

--- API/WebApi/WebApi/Startup.cs
+++ API/WebApi/WebApi/Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
 using WebApi.Commands.Instance;
 using WebApi.Commands.Interface;
 using WebApi.Services.Instance;
@@ -38,6 +41,32 @@ namespace WebApi
                 // 停用ModelStateInvalidFilter => 使用自訂ValidFilter:ValidRequest
                 opt.SuppressModelStateInvalidFilter = true;
             });
+
+            #region 註冊Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    // name: 攸關 SwaggerDocument 的 URL 位置。
+                    name: "v1",
+                    // info: 是用於 SwaggerDocument 版本資訊的顯示(內容非必填)。
+                    info: new OpenApiInfo
+                    {
+                        Title = "WebApi",
+                        Version = "1.0.0",
+                        Description = "This is ASP.NET Core API Sample.",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Matrix",
+                            Email = string.Empty
+                        }
+                    }
+                );
+
+                // XML 檔案: 文件註解標籤
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, "WebApi.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +87,19 @@ namespace WebApi
             {
                 endpoints.MapControllers();
             });
+
+            #region 使用SwaggerUI
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(
+                    // url: 需配合 SwaggerDoc 的 name。 "/swagger/{SwaggerDoc name}/swagger.json"
+                    url: "/swagger/v1/swagger.json",
+                    // name: 用於 Swagger UI 右上角選擇不同版本的 SwaggerDocument 顯示名稱使用。
+                    name: "API Document V1"
+                );
+            });
+            #endregion
         }
     }
 }
